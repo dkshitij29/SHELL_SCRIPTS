c60738df7442e3306b6ac8956099ae4c9ef00f46
dkshitij29 <111069156+dkshitij29@users.noreply.github.com>
2022-12-10 20:14:29 +0530

    Add files via upload

---
+++ all_apps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+brew update #brew's internal list of formulas to the latest
+brew upgrade #To upgrade all outdated formulas
+brew install --cask android-studio #for android studio
+brew install --cask xampp #Xamp server
+brew install --cask discord #Discord
+brew install docker #Docker
+brew install --cask firefox #firefox "Need to developer edition"
+brew install --cask libreoffice #libre_office
+brew install --cask protonvpn #vpn
+brew install --cask signal #signal
+brew install --cask stremio #stremio
+brew install --cask telegram #telegram
+brew install --cask thunderbird #thunderbird
+brew install --cask utm #UTM vm manager
+brew install --cask vlc #vlc media player
+brew install --cask vscodium # vscodium
+brew install --cask weka # weka data visualization tool
+brew install --cask whatsapp #whatsapp
---
+++ brew.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Do give Exegutable access by command "chmod +x brew.sh"
